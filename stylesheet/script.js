b4d6eb20d65f4ea2814dec5e8c9ee750c5d4ff2a
fuadbla008 <171414330+Fuadbla008@users.noreply.github.com>
2025-07-02 21:37:32 +0600

    update branch

--- stylesheet/script.js
+++ stylesheet/script.js
@@ -4,9 +4,7 @@ const divs = document.querySelectorAll('.block');
   divs.forEach(div => {
     div.addEventListener('click', () => {
       const divvalue = div.innerHTML;
-    //   console.log(divvalue);
-    return divvalue;
+      console.log(divvalue);
     });
   });
 
-console.log(divvalue);
