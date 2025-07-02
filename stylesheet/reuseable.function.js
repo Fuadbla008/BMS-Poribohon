f2a649a297a49776739cce1fdb4ac69a40071114
fuadbla008 <171414330+Fuadbla008@users.noreply.github.com>
2025-07-02 22:52:48 +0600

    succesfully appended child div

--- stylesheet/reuseable.function.js
+++ stylesheet/reuseable.function.js
@@ -2,3 +2,30 @@ function setTextById(id, data){
     const text = document.getElementById(id);
     text.innerText = data;
 }
+
+function setBgColorById(id, color){
+    const element = document.getElementById(id);
+    element.style.backgroundColor = color;
+}
+
+function appendDivById(id, idForContainer) {
+    const newDiv = document.createElement('div');
+    newDiv.className = 'flex-between';
+    // newDiv.className += 'appendPrice';
+
+    const span1 = document.createElement('span');
+    span1.textContent = id;
+
+    const span2 = document.createElement('span');
+    span2.textContent = 'economy';
+    const span3 = document.createElement('span');
+    span3.textContent = '550';
+    span3.className = 'appendPrice';
+
+    newDiv.appendChild(span1);
+    newDiv.appendChild(span2);
+    newDiv.appendChild(span3);
+
+    document.getElementById(idForContainer).appendChild(newDiv);
+
+}
--- stylesheet/script.js
+++ stylesheet/script.js
@@ -1,10 +1,20 @@
 
 const divs = document.querySelectorAll('.block');
 
-  divs.forEach(div => {
+divs.forEach(div => {
     div.addEventListener('click', () => {
-      const divvalue = div.innerHTML;
-      console.log(divvalue);
+        const divvalue = div.innerHTML;
+        console.log(divvalue);
+        setBgColorById(divvalue, '#1DD100')
+
+        appendDivById(divvalue, 'appendchild');
+
+        const price = document.querySelectorAll('.appendPrice');
+        price.forEach(price => {
+            // Assuming the price is in the third span of each div
+            const ticketPrice =parseInt(price.innerHTML);
+            console.log(ticketPrice);
+        });
     });
-  });
+});
 
