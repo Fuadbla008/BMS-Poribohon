9aa6a0f8f682e4ae4493ad009e78ba1d4a06bfa0
fuadbla008 <171414330+Fuadbla008@users.noreply.github.com>
2025-07-03 14:16:37 +0600

    updated selected button condition

--- script.js
+++ script.js
@@ -22,20 +22,25 @@ divs.forEach(div => {
 // discount price function
 function discountPrice() {
     const inputValue = getInputValueById('couponNumber');
+    let countTicket = elementById('selectedSeat');
+    
     if (inputValue === 'New15') {
         const totalPrice = elementById('totalPrice');
         const discount = totalPrice * 0.15;
         const grandTotal = totalPrice - discount;
-        console.log(grandTotal);
+        // console.log(grandTotal);
         setTextById('grandTotal', grandTotal);
     }
-    else if (inputValue === 'Couple20') {
+    
+    else if (inputValue === 'New20' && countTicket >= 2) {
         const totalPrice = elementById('totalPrice');
         const discount = totalPrice * 0.20;
         const grandTotal = totalPrice - discount;
-        console.log(grandTotal);
+        // console.log(grandTotal);
         setTextById('grandTotal', grandTotal);
     }
-
+    else{
+        alert('Invalid coupon code or not enough tickets selected for discount.');
+    }
 
 }
